992176527214926ba58b35dd088b344b07f6ea89
Bruna Manoela <162349937+Brunarenner@users.noreply.github.com>
2024-09-04 21:51:26 -0300

    Added h1 tag.

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+   {
+    titulo:"Rayssa Leal",
+        descrição:"Rayssa Leal, a Fadinha, é uma skatista brasileira que, com apenas 13 anos, conquistou a medalha de prata nos Jogos Olímpicos de Tóquio 2020. Sua habilidade e carisma a tornaram um ícone do esporte.",
+        link:'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+        instagran:'https://www.instagram.com/rayssalealsk8'
+    },
+    {
+        titulo:"Marta",
+            descrição:"Marta Vieira da Silva, a Rainha, é considerada uma das maiores jogadoras de futebol de todos os tempos. Com sua habilidade e liderança, ela conquistou diversos títulos e deixou seu nome marcado na história do futebol feminino.",
+            link:'https://pt.wikipedia.org/wiki/Marta_Vieira_da_Silva',
+            instagran:'https://www.instagram.com/martasvieirada/?hl=pt-br'
+        },
+        {
+            titulo:"Rebeca Andrade",
+                descrição:"Rebeca Andrade, a joia da ginástica artística brasileira, tem escrito seu nome nos anais do esporte com letras de ouro. Com uma combinação única de força, flexibilidade edeterminação, ela conquistou o mundo ao se tornar a maior medalhista olímpica brasileira de todos ostempos.",
+                link:'https://www.instagram.com/rebecarandrade/?hl=pt-br',
+                instagran:'https://www.instagram.com/rebecarandrade/?hl=pt-br'
+            },
+        
+    
+
+]
