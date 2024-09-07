74c3230027aed0e64d295e9995818ce4531bd790
Bruna Manoela <162349937+Brunarenner@users.noreply.github.com>
2024-09-06 23:33:07 -0300

    . modified:   app.js On branch main

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log(dados);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa") .value
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>🤷‍♀️Nada Foi Encontrado🤷‍♀️</p>"
+        return
+    }
+  campoPesquisa = campoPesquisa.toLowerCase()
+  
+let resultados = "";
+let titulo = "";
+let descrição = "";
+let tags = "";
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLocaleLowerCase()
+        descrição = dado.descrição.toLocaleLowerCase()
+        tags = dado.tags.toLocaleLowerCase()
+        if (titulo.includes(campoPesquisa) ||descrição.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+    resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descrição-meta">${dado.descrição}</p>
+          <a href="${dado.link}" target="_blank">Mais Informações</a>
+        </div>
+      `;
+    }
+}
+if (!resultados){
+    resultados = "<p>Nada Foi Encontrado</p>"
+
+}
+
+section.innerHTML = resultados;
+}
